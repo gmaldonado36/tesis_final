4d3d1aef151da92c7606b9fffdbec93016373699
Gerardo Maldonado <gmaldonadopoinsot@gmail.com>
2026-02-11 20:07:06 -0600

    a

--- pages/end.py
+++ pages/end.py
@@ -4,10 +4,13 @@ from services.excel_writer import write_to_google_sheets
 
 def run():
     st.title("Finalizado")
-
     st.success("Gracias por completar el experimento")
 
-    excel = write_to_google_sheets()
+    # Evitar que se escriba múltiples veces
+    if "saved_to_sheets" not in st.session_state:
+        write_to_google_sheets()
+        st.session_state.saved_to_sheets = True
+        st.success("Resultados guardados en Google Sheets ✅")
 
 
 
