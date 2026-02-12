3a0e5d21f92d8f10f570d6dfa63d7e700a168126
Gerardo Maldonado <gmaldonadopoinsot@gmail.com>
2026-02-11 19:56:40 -0600

    google

--- services/excel_writer.py
+++ services/excel_writer.py
@@ -1,10 +1,26 @@
 import pandas as pd
-from io import BytesIO
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
-def generate_excel():
+SHEET_NAME = "DataTesis"  # <-- cambia esto
+
+
+def connect_to_sheets():
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive",
+        ],
+    )
+    client = gspread.authorize(creds)
+    return client
+
+
+def write_to_google_sheets():
     data = {
-        "nombre_usuario": [st.session_state.nombre]*len(st.session_state.imagenes),
+        "nombre_usuario": [st.session_state.nombre] * len(st.session_state.imagenes),
         "imagen": st.session_state.imagenes,
         "tiempo_segundos": st.session_state.tiempos,
         "escala": st.session_state.escalas,
@@ -12,7 +28,8 @@ def generate_excel():
 
     df = pd.DataFrame(data)
 
-    buffer = BytesIO()
-    df.to_excel(buffer, index=False)
-    buffer.seek(0)
-    return buffer
+    client = connect_to_sheets()
+    sheet = client.open(SHEET_NAME).sheet1
+
+    # Si quieres agregar sin borrar:
+    sheet.append_rows(df.values.tolist())
