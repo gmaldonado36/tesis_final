22cc72f3647f8ad1f7cc6b7fe290147d91554dba
Gerardo Maldonado <gmaldonadopoinsot@gmail.com>
2026-02-11 20:00:21 -0600

    fix

--- pages/end.py
+++ pages/end.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from services.excel_writer import generate_excel
+from services.excel_writer import write_to_google_sheets
+
 
 def run():
     st.title("Finalizado")
 
     st.success("Gracias por completar el experimento")
 
-    excel = generate_excel()
+    excel = write_to_google_sheets()
+
 
     st.download_button(
         label="Descargar resultados",
